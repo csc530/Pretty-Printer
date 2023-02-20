05490200e601eb680355162dbdf7fc8f3a2de865
csc530 <77406318+csc530@users.noreply.github.com>
2023-02-20 12:19:22 -0500

    add documentation to class methods and members

--- Pretty Printer/PrettyConsole.vb
+++ Pretty Printer/PrettyConsole.vb
@@ -1,9 +1,10 @@
-Option Strict On
-
 Imports System.Drawing
 Imports System.Text
 Imports System.Threading
 
+''' <summary>
+''' <see cref="Console"/> wrapper class extending its features with virtual terminal sequences
+''' </summary>
 Public Class PrettyConsole
 	Private Enum ConsoleVirtualTerminalSequences
 		'https://learn.microsoft.com/en-us/windows/console/console-virtual-terminal-sequences#simple-cursor-positioning
@@ -22,10 +23,19 @@ Public Class PrettyConsole
 		TextBackground
 		TextColour
 	End Enum
-
+	''' <summary>
+	''' Starting escape sequence to enter virtual terminal sequences
+	''' </summary>
 	Shared ReadOnly SequenceStart As String = $"{Convert.ToChar(ConsoleKey.Escape)}["
+	''' <summary>
+	''' List of modifications to console output
+	''' </summary>
+	''' <returns>A set of changes to the console output</returns>
 	Private Property ConsoleModifications As HashSet(Of ConsoleVirtualTerminalSequences)
-
+	''' <summary>
+	''' The virtual terminal sequences to modify the console output
+	''' </summary>
+	''' <returns>A string of the vts to modify subsequent outputs</returns>
 	Private Function GetVirtualSequences() As String
 		Dim sequences = New StringBuilder()
 		For Each sequence In ConsoleModifications
@@ -72,14 +82,16 @@ Public Class PrettyConsole
 					sequences.Remove(sequences.Length - SequenceStart.Length, SequenceStart.Length)
 					REM My.Application.Log can`t find log on `my`
 
-
 			End Select
 		Next
 		Return sequences.ToString()
 	End Function
 
 	Private _underline As Boolean
-
+	''' <summary>
+	''' Underline printed console text
+	''' </summary>
+	''' <returns><see cref="Boolean"/> if the current output will underlined</returns>
 	Public Property Underline As Boolean
 		Get
 			Return _underline
@@ -92,10 +104,13 @@ Public Class PrettyConsole
 
 	Private _backgroundColour As Color
 
+	''' <summary>
+	''' The output's background colour; does NOT SUPPORT TRANSPARENCY in colours
+	''' </summary>
 	''' <value>
 	''' The background colour;
-	''' does NOT SUPPORT TRANSPARENCY in colours
 	''' </value>
+	''' <returns></returns>
 	Public Property BackgroundColour As Color
 		Get
 			Return _backgroundColour
@@ -123,21 +138,19 @@ Public Class PrettyConsole
 	End Property
 
 
-	'''
 	''' <summary>Creates a pretty console object that writes to <see cref="System.Console"/> output</summary>
 	''' <exception cref="NotSupportedException">If the environment console does not support Virtual Terminal Sequences<seealso cref="https://learn.microsoft.com/en-us/windows/console/console-virtual-terminal-sequences"/></exception>
-	''' 
 	Sub New()
 		ConsoleModifications = New HashSet(Of ConsoleVirtualTerminalSequences)
-		' other enum values said they weren't in use 🤷🏿‍♂️
-		If PlatformID.Win32NT = Environment.OSVersion.Platform Then
+		' other enum values they said weren't in use for windows to activate vts 🤷🏿‍♂️
+		If Environment.OSVersion.Platform = PlatformID.Win32NT Then
 
 			'? Enable virtual terminal sequences
 			Dim stdHandle = WindowsConsoleApi.GetStdHandle(WindowsConsoleApi.StdHandles.StdInputHandle)
 			Dim consoleMode As WindowsConsoleApi.ConsoleModes
 			WindowsConsoleApi.GetConsoleMode(stdHandle, consoleMode)
 			Const compatibleConsoleModes = WindowsConsoleApi.ConsoleModes.EnableVirtualTerminalProcessing Or
-			                               WindowsConsoleApi.ConsoleModes.EnableProcessedOutput
+										   WindowsConsoleApi.ConsoleModes.EnableProcessedOutput
 			If _
 				Not consoleMode.HasFlag(compatibleConsoleModes) AndAlso
 				Not WindowsConsoleApi.SetConsoleMode(stdHandle, compatibleConsoleModes) _
@@ -148,7 +161,7 @@ Public Class PrettyConsole
 	End Sub
 
 
-	''' <summary>Resets this all customization to this console printer</summary>
+	''' <summary>Resets all customization made to this pretty console</summary>
 	Sub Reset()
 		BackgroundColour = Nothing
 		TextColour = Nothing
@@ -167,17 +180,27 @@ Public Class PrettyConsole
 
 #Region "Print"
 
+
+	''' <inheritdoc cref="PrintLine(String)"/>
+	''' <param name="backgroundColour">The background colour of the text</param>
+	''' <param name="textColour">The colour of the text</param>
+	''' <param name="underline">underline the text</param>
 	Public Sub PrintLine(value As String, Optional backgroundColour As Color = Nothing,
-	                     Optional textColour As Color = Nothing, Optional underline As Boolean = False)
+						 Optional textColour As Color = Nothing, Optional underline As Boolean = False)
 		Print(value & Environment.NewLine, backgroundColour, textColour, underline)
 	End Sub
 
+	''' <summary>
+	''' Print given text with an appended new line character to the console
+	''' </summary>
+	''' <param name="value">the text to print</param>
 	Sub PrintLine(value As String)
 		Print(value & Environment.NewLine)
 	End Sub
 
+	'''<inheritdoc cref="Print(String)"/>
 	Sub Print(value As String, Optional backgroundColour As Color = Nothing, Optional textColour As Color = Nothing,
-	          Optional underline As Boolean = False)
+			  Optional underline As Boolean = False)
 
 		Dim previous = (Me.BackgroundColour, Me.TextColour, underline)
 		Me.BackgroundColour = backgroundColour
@@ -190,14 +213,17 @@ Public Class PrettyConsole
 		Me.Underline = previous.underline
 	End Sub
 
+	''' <summary>Print the givent text to the console output</summary>
+	''' <inheritdoc cref="PrintLine(String, Color, Color, Boolean)"/>
 	Sub Print(value As String)
 		System.Console.Write(GetVirtualSequences() & value)
 	End Sub
 
-
+	'''<summary>Print to the console ending with a new line character at a set speed; imitates typing text</summary>
+	'''<inheritdoc cref="SlowPrint(String, Integer, Color, Color, Boolean, PrintUnit)"/>
 	Sub SlowPrintLine(value As String, Optional speed As Integer = 15,
-	                  Optional backgroundColour As Color = Nothing, Optional textColour As Color = Nothing,
-	                  Optional underline As Boolean = False, Optional unitOfSpeed As PrintUnit = PrintUnit.Character)
+					  Optional backgroundColour As Color = Nothing, Optional textColour As Color = Nothing,
+					  Optional underline As Boolean = False, Optional unitOfSpeed As PrintUnit = PrintUnit.Character)
 		SlowPrint(value & Environment.NewLine, speed, backgroundColour, textColour, underline, unitOfSpeed)
 	End Sub
 
@@ -210,16 +236,14 @@ Public Class PrettyConsole
 	''' <param name="textColour">The text colour.</param>
 	''' <param name="underline">if set to <c>true</c> underlines the text.</param>
 	''' <param name="unitOfSpeed">The unit to print per second.</param>
-	Sub SlowPrint(value As String, Optional speed As Integer = 15,
-	              Optional backgroundColour As Color = Nothing, Optional textColour As Color = Nothing,
-	              Optional underline As Boolean = False, Optional unitOfSpeed As PrintUnit = PrintUnit.Character)
+	Sub SlowPrint(value As String, Optional speed As Integer = 15, Optional backgroundColour As Color = Nothing, Optional textColour As Color = Nothing, Optional underline As Boolean = False, Optional unitOfSpeed As PrintUnit = PrintUnit.Character)
 		' * made it blocking as if they try to print or modify the classes values not errors are thrown, sry🤷🏿‍♂️
 		Select Case unitOfSpeed
 			Case PrintUnit.Character
 				' i.e. the x^-1 inverse because were' sleeping it and not setting the speed
 				' ex. speed 2chars/sec => sleep = 500 = half a second not 2000 = 4chars/sec
 				' by 1000 to convert to milliseconds
-				speed = Convert.ToInt32(Math.Round(1000/speed, 0))
+				speed = Convert.ToInt32(Math.Round(1000 / speed, 0))
 				_printSlow(value, speed, backgroundColour, textColour, underline)
 			Case PrintUnit.Line
 				For Each line In value.Split(Environment.NewLine)
@@ -234,7 +258,7 @@ Public Class PrettyConsole
 	End Sub
 
 	Private Sub _printSlow(value As String, speed As Integer, backgroundColour As Color, textColour As Color,
-	                       underline As Boolean)
+						   underline As Boolean)
 		Dim character As Char
 		For index = 0 To value.Length - 1
 			character = value(index)
@@ -246,7 +270,7 @@ Public Class PrettyConsole
 				index += 1
 				'todo change cursor to underline just not block makes newline jump jarring
 				If index + 1 <> value.Length - 1 Then
-					Thread.Sleep(Convert.ToInt32((285*speed/67)))
+					Thread.Sleep(Convert.ToInt32((285 * speed / 67)))
 				End If
 			Else
 				Thread.Sleep(speed)
@@ -261,7 +285,7 @@ Public Class PrettyConsole
 		If colour.Name = "0" Then
 			Return False
 		Else
-			Return [Enum].TryParse (Of ConsoleColor)(colour.Name, True, Nothing)
+			Return [Enum].TryParse(Of ConsoleColor)(colour.Name, True, Nothing)
 		End If
 	End Function
 End Class
